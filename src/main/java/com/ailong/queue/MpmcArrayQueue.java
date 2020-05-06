b941a6055b8ee0ea88d5590cd22209faee9e3201
亚克(蒋飞) <yake.jiangf@goparcel.cn>
2020-05-06 23:04:04 +0800

    修改Mpsc

---
+++ src/main/java/com/ailong/queue/MpmcArrayQueue.java
@@ -0,0 +1,9 @@
+package com.ailong.queue;
+
+/**
+ * 无锁有界队列
+ * 多生产者---多消费者
+ * Created by jiangfei on 2020/5/6.
+ */
+public class MpmcArrayQueue<E> {
+}
---
+++ src/main/java/com/ailong/queue/SpmcArrayQueue.java
@@ -0,0 +1,9 @@
+package com.ailong.queue;
+
+/**
+ * 无锁 有界队列
+ * 单生产者--多消费者
+ * Created by jiangfei on 2020/5/6.
+ */
+public class SpmcArrayQueue<E> {
+}
